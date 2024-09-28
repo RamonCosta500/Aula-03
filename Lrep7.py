9bc18fdbd9ffa0e85a1c1529a47b1f5d438ac75d
Pedra123 <luisfelipe.de.araujo2005@gmail.com>
2024-09-28 17:12:01 -0300

    alt1

--- Lrep7.py
+++ Lrep7.py
@@ -11,8 +11,8 @@ while numero != 0:
 ordem = input("Deseja ordem crescente ou decrescente ? :")
 
 if ordem == "decrescente":
-    for i in range(len(lista)):
-        while lista[i] <= lista[i + 1]:
+    contato = range(len(lista)):
+        if lista[i] <= lista[i + 1]:
             c = lista[i]
             lista[i] = lista[i + 1]
             lista[i + 1] = c
